09f147a7439b75307a83af113ea3d65cd99c0a19
Alexander Sharapov <shyrimon2000@gmail.com>
2026-05-24 23:24:48 +0300

    Added new variable 'roles' to user response schema

--- app/schemas.py
+++ app/schemas.py
@@ -18,6 +18,7 @@ class UserResponse(BaseModel):
     id: int
     username: str
     email: EmailStr
+    role: str
     is_active: bool
     created_at: datetime
     last_seen_at: datetime | None = None
