955e8a6225616f1d39a4cb170dac3765bb9c7ec5
Alexander Sharapov <shyrimon2000@gmail.com>
2026-05-24 23:23:54 +0300

    Added new row 'roles' to user table

--- app/models.py
+++ app/models.py
@@ -13,6 +13,8 @@ class User(Base):
     email = Column(String(255), unique=True, index=True, nullable=False)
     hashed_password = Column(String(255), nullable=False)
 
+    role = Column(String(20), default="user", nullable=False)
+
     is_active = Column(Boolean, default=True, nullable=False)
 
     created_at = Column(DateTime(timezone=True), server_default=func.now())
